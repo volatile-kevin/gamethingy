f015cedfe4cb1000eccbe28a6fa3b80e4f0e8dbb
Kevin Hu <kwh2@illinois.edu>
2018-10-18 21:30:18 -0500

    I dont know how to use git

--- mp6/updateBoard.c
+++ mp6/updateBoard.c
@@ -1,3 +1,14 @@
+/*Jose Lopez - joseal2
+*Partners: Daniel Ao - dao3, Kevin Hu - kwh2
+*This program updates a gameboard based on Conway's game of life.
+*The three functions are called by main.c and provide the status of the board
+*and what should be update/changed. The board is represented by an array that
+*acts as a 2 dimensional gameboard, with 1's representing live cells and 0's
+*representing dead ones.
+*/
+
+
+
 /*
  * countLiveNeighbor
  * Inputs:
@@ -13,9 +24,11 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
-	int alive = 0;
-	int position = row*boardColSize+col;
 
+	int alive = 0; //start with the number of alive neighbors at 0
+	int position = row*boardColSize+col; //mark the position of element in 1D array
+
+	//If this is not the top row, check the three elements above
 	if(row-1>=0){
 		if(col-1>=0 && board[position-boardColSize-1]==1)
 			alive++;
@@ -25,11 +38,13 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
 			alive++;
 	}
 
+	//Check the left and right elements
 	if(col-1>=0 && board[position-1]==1)
 		alive++;
 	if(col+1 < boardColSize && board[position+1]==1)
 		alive++;
 
+	//If element is not in bottom row, check the three elements below
 	if(row+1<boardRowSize){
 		if(col-1>=0 && board[position+boardColSize-1]==1)
 			alive++;
@@ -52,20 +67,30 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+	//Create a temporary board to read the past status of the elements
 	int temp[boardColSize*boardRowSize];
-	int lol;
-	int size = boardColSize*boardRowSize;
+
+	int lol; //silly counter for the for first for loop
+	int size = boardColSize*boardRowSize; //get the size of the array in 1D
+
+	//copy current board status into temp[]
 	for(lol = 0; lol<size; lol++){
 		temp[lol]=board[lol];
 	}
+
+	//Update the board based on how many alive cells surround each element
 	int i;
 	for(i = 0; i < size; i++){
+		//If element is alive and has 2 or 3 live neighbors, keep it alive
 		if(temp[i]==1){
-			if(countLiveNeighbor(temp,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==2 || countLiveNeighbor(temp,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==3)
+			if(countLiveNeighbor(temp,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==2 ||
+				 countLiveNeighbor(temp,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==3)
 				board[i]= 1;
 			else
 				board[i]= 0;
-		}else{
+		}
+		//If element is dead and has 3 live neighbors, bring it back to life
+		else{
 			if(countLiveNeighbor(temp,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==3)
 				board[i]= 1;
 			else
@@ -87,13 +112,17 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * return 0 if the alive cells change for the next step.
  */
 int aliveStable(int* board, int boardRowSize, int boardColSize){
-	int size = boardColSize*boardRowSize;
-	int flag = 1;
+	int size = boardColSize*boardRowSize; //size of board in 1D
+	int flag = 1; //flag gets set to 0 if the board needs to change
 	int i;
+	//Parse through the game board
 	for(i=0;i<size;i++){
+		//If element is dead but has three live neighbors, gameboard must be updated
 		if(board[i]==0 && countLiveNeighbor(board,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==3)
 			flag = 0;
-		if(board[i]==1 && !(countLiveNeighbor(board,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==2 || countLiveNeighbor(board,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==3))
+		//If element is alive and doesn't have 2 or 3 live neighbors, the gameboard must be updated
+		if(board[i]==1 && !(countLiveNeighbor(board,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==2 ||
+												countLiveNeighbor(board,boardRowSize,boardColSize,i/boardColSize,i%boardColSize)==3))
 			flag = 0;
 	}
 	return flag;

58e5034751976515a419ef03c9b915e38e1370f8
Kevin <kwhu5695@gmail.com>
2020-02-23 19:11:13 -0600

    initial

--- mp6/updateBoard.h
+++
@@ -1,3 +0,0 @@
-void updateBoard(int* board, int boardRowSize, int boardColSize);
-int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col);
-int aliveStable(int* board, int boardRowSize, int boardColSize);
--- mp6/updateBoardGold.h
+++
@@ -1,3 +0,0 @@
-void updateBoard_gold(int* board, int boardRowSize, int boardColSize);
-int countLiveNeighbor_gold(int* board, int boardRowSize, int boardColSize, int row, int col);
-int aliveStable_gold(int* board, int boardRowSize, int boardColSize);
